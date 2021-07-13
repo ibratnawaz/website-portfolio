c57f416c7c7e537db27fdb91e50494b4d78de788
Ibrat Nawaz <ibratrocker99@gmail.com>
2021-07-13 16:42:30 +0530

    Created about me section

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import AboutMe from './pages/AboutMe';
+import MyWork from './pages/MyWork';
+import ContactMe from './pages/ContactMe';
 
 function App() {
-  return <div className='App'>My Portfolio 2.0</div>;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path='/' component={AboutMe} />
+        <Route exact path='/work' component={MyWork} />
+        <Route exact path='/contact' component={ContactMe} />
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
