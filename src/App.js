3d3d631f1c7b0c1324e6ba9ddfc073ea202bb0e8
Ibrat Nawaz <ibratrocker99@gmail.com>
2021-07-15 21:06:57 +0530

    Created Navbar

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import AboutMe from './pages/AboutMe';
 import MyWork from './pages/MyWork';
 import ContactMe from './pages/ContactMe';
+import Navbar from './components/Navbar';
 
 import GlobalStyle from './components/GlobalStyle';
 import 'remixicon/fonts/remixicon.css';
@@ -12,6 +13,7 @@ function App() {
   return (
     <Router>
       <GlobalStyle />
+      <Navbar />
       <Switch>
         <Route exact path='/' component={AboutMe} />
         <Route exact path='/work' component={MyWork} />
