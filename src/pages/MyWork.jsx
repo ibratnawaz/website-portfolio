92a303a6b6ba9fa35c67ae5a4f26b228e1f79cb2
Ibrat Nawaz <ibratrocker99@gmail.com>
2021-07-17 20:50:24 +0530

    Added page transition to my work section

--- src/pages/MyWork.jsx
+++ src/pages/MyWork.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { motion } from 'framer-motion';
+import styled from 'styled-components';
 
-import { pageAnimation } from '../animation';
+import { pageAnimation, slider, sliderContainer } from '../animation';
 
 const MyWork = () => {
   return (
@@ -10,9 +11,34 @@ const MyWork = () => {
       initial='hidden'
       animate='show'
       exit='exit'>
+      <motion.div variants={sliderContainer} style={{ overflow: 'hidden' }}>
+        <Frame1 variants={slider}></Frame1>
+        <Frame2 variants={slider}></Frame2>
+        <Frame3 variants={slider}></Frame3>
+        <Frame4 variants={slider}></Frame4>
+      </motion.div>
       MyWork
     </motion.div>
   );
 };
+
+const Frame1 = styled(motion.div)`
+  position: fixed;
+  left: 0;
+  top: 10%;
+  width: 100%;
+  height: 100vh;
+  background: #fffebf;
+  z-index: 2;
+`;
+const Frame2 = styled(Frame1)`
+  background: #ff8efb;
+`;
+const Frame3 = styled(Frame1)`
+  background: #8ed2ff;
+`;
+const Frame4 = styled(Frame1)`
+  background: #8effa0;
+`;
 
 export default MyWork;
