55bab5c2550daeab7434531131e0828d382f8f17
Ibrat Nawaz <ibratrocker99@gmail.com>
2021-07-20 11:30:10 +0530

    Added css for scroll bar

--- src/components/GlobalStyle.jsx
+++ src/components/GlobalStyle.jsx
@@ -9,6 +9,20 @@ const GlobalStyle = createGlobalStyle`
 
 
 html{
+    ::-webkit-scrollbar {
+        width: 0.3rem;
+    }
+        ::-webkit-scrollbar-track {
+        box-shadow: inset 0 0 5px grey;
+        border-radius: 10px;
+    }
+        ::-webkit-scrollbar-thumb {
+        background: #F9004D;
+        border-radius: 10px;
+    }
+        ::-webkit-scrollbar-thumb:hover {
+        background: grey;
+    }
     @media (max-width: 1700px){
         font-size: 75%;
     }
